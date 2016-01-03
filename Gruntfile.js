b52319ed27a08def9be97a1e7d394590f330dc86
Alexander Pekkari <pekkarih@gmail.com>
2016-01-03 20:22:18 +0100

    looser linting, add demo pages and basic structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,29 +4,53 @@ var postcss = function(){
 
 module.exports = function(grunt) {
   grunt.initConfig({
+    jade: {
+      demo: {
+        options: {
+          client: false,
+          pretty: true
+        },
+        files: [ {
+          cwd: "src/tpl",
+          src: "**/*.jade",
+          dest: "dist",
+          expand: true,
+          ext: ".html"
+        } ]
+      }
+    },
     stylus: {
       compile: {
         options: {
           use: [postcss]
         },
         files: {
-          'dist/stylesheets/index.css': 'src/stylesheets/index.styl'
+          'dist/style/glisten.css': 'src/style/index.styl',
+          'dist/style/demo.css': 'src/style/demo.styl'
         }
       }
     },
     watch: {
-      styles: {
-        files: ['src/stylesheets/*.*'],
+      style: {
+        files: ['src/style/*.*'],
         tasks: ['stylus'],
         options: {
           spawn: false,
-        },
+        }
+      },
+      jade: {
+        files: ['src/**/*.jade'],
+        tasks: ['jade'],
+        options: {
+          spawn: false,
+        }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-stylus');
   grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks('grunt-contrib-jade');
 
   grunt.registerTask("default", ["watch"]);
   grunt.registerTask("cssnext", ["cssnext"]);
